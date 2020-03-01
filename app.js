d0a2a869b7c85b3e23078ba12e50cd91cdd24e4f
Crispina Muriel <47477912+crispinamuriel@users.noreply.github.com>
2020-03-01 06:27:22 -0500

    notes

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-class SignUp extends React.Component {
-    constructor(props) {
-        super(props);
+class SignUp extends React.Component {  //1 define Parent class app
+    constructor(props) {  
+        super(props); //3 getting props ability from React
         this.state = {
-            firstName: '',
+            firstName: '',    //4 define state
             lastName: '',
             email: '',
             password: ''
         }
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);    //6  bind methods to use 'this' keyword inside   : 'this.setState({})
         this.handleChange = this.handleChange.bind(this);
     }
-    handleSubmit (e) {
+    handleSubmit (e) {                         //5 define methods to use in JSX or to pass as props to child classes
         e.preventDefault();
         console.log(this.state);
     }
     handleChange (e) {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value     
         });
     }
-    render() {
+    render() {          //7 render method returns JSX
         return(
-            <div className="container">
-                <form onSubmit={this.handleSubmit}>
-                   <span>First Name</span> <input name="firstName" onChange={this.handleChange} placeholder="first name"></input> <br /> 
-                   <span>Last Name</span> <input name="lastName" onChange={this.handleChange} placeholder="last name"></input> <br /> 
-                   <span>Email:</span> <input name="email" onChange={this.handleChange} placeholder="email"></input> <br /> 
-                   <span>Password:</span><input type="password" name="password" onChange={this.handleChange} placeholder="password"></input> <br /> 
+            <div className="container">        {/* JSX must be returned inside a single parent element (usually a div) or React.Fragment*/}
+                <form onSubmit={this.handleSubmit}>  {/*methods will be found on this. or this.props */}
+                   <span>First Name</span> <input name="firstName" onChange={this.handleChange} placeholder="first name"></input> <br />  {/*span is an inline element*/}
+                   <span>Last Name</span> <input name="lastName" onChange={this.handleChange} placeholder="last name"></input> <br />     {/*input name is required to make a controlled form*/}
+                   <span>Email:</span> <input name="email" onChange={this.handleChange} placeholder="email"></input> <br />               {/*if method was from parent, found on this.props.methodName*/}
+                   <span>Password:</span><input type="password" name="password" onChange={this.handleChange} placeholder="password"></input> <br />  {/*to obfuscate pw put type as password*/}
                    <button type="submit">Sign Up</button>
                 </form>
             </div>
@@ -34,4 +34,4 @@ class SignUp extends React.Component {
     }
 }
 
-ReactDOM.render(<SignUp />, document.getElementById('app'));
+ReactDOM.render(<SignUp />, document.getElementById('app')); //2 using rDOM to render app in HTML file
